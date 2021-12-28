5a81955cedfca2348dccc2029d428da9dd4906fb
lanice <1871704+lanice@users.noreply.github.com>
2021-12-28 11:29:37 +0100

    feat: add initial support for inline rant blocks

--- src/main.ts
+++ src/main.ts
@@ -1,60 +1,124 @@
-import { Plugin, Notice, MarkdownView, TFile } from "obsidian";
+import {
+  Plugin,
+  Notice,
+  MarkdownView,
+  TFile,
+  MarkdownPostProcessorContext,
+} from "obsidian";
 // @ts-ignore
 import rustPlugin from "../pkg/obsidian_rantlang_plugin_bg.wasm";
 import { around } from "monkey-around";
 import init from "../pkg/obsidian_rantlang_plugin.js";
-import RantProcessor from "./processor";
+import RantProcessor, { InlineRantProcessor } from "./processor";
 import { randomSeed } from "./utils";
 
+type RantProcessorType = RantProcessor | InlineRantProcessor;
+
 export default class RantLangPlugin extends Plugin {
-  fileMap: Map<TFile, RantProcessor[]> = new Map();
+  fileMap: Map<TFile, RantProcessorType[]> = new Map();
 
   async onload() {
     const buffer = Uint8Array.from(atob(rustPlugin), (c) => c.charCodeAt(0));
     await init(Promise.resolve(buffer));
 
-    this.registerMarkdownCodeBlockProcessor("rant", (source, el, ctx) => {
-      const file = this.app.vault.getAbstractFileByPath(ctx.sourcePath);
-      if (!file || !(file instanceof TFile)) return;
+    this.registerMarkdownCodeBlockProcessor(
+      "rant",
+      (source: string, el: HTMLElement, ctx: MarkdownPostProcessorContext) => {
+        const file = this.app.vault.getAbstractFileByPath(ctx.sourcePath);
+        if (!file || !(file instanceof TFile)) return;
 
-      const processor = new RantProcessor(
-        source,
-        el.createDiv({ cls: "rant" })
-      );
-      processor.rant(randomSeed());
+        const processor = new RantProcessor(
+          source,
+          el.createDiv({ cls: "rant" })
+        );
+        processor.rant(randomSeed());
 
-      /* File-based tracking of registered processors inspired by javalent's excellent dice roller plugin: https://github.com/valentine195/obsidian-dice-roller */
+        /* File-based tracking of registered processors inspired by javalent's excellent dice roller plugin: https://github.com/valentine195/obsidian-dice-roller */
 
-      if (!this.fileMap.has(file)) {
-        this.fileMap.set(file, []);
+        if (!this.fileMap.has(file)) {
+          this.fileMap.set(file, []);
+        }
+        this.fileMap.set(file, [...this.fileMap.get(file), processor]);
+
+        const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+        if (
+          view &&
+          this.fileMap.has(file) &&
+          this.fileMap.get(file).length === 1
+        ) {
+          const self = this;
+
+          let unregisterOnUnloadFile = around(view, {
+            onUnloadFile: function (next) {
+              return async function (unloaded: TFile) {
+                if (unloaded == file) {
+                  self.fileMap.delete(file);
+                  unregisterOnUnloadFile();
+                }
+
+                return await next.call(this, unloaded);
+              };
+            },
+          });
+
+          view.register(unregisterOnUnloadFile);
+          view.register(() => this.fileMap.delete(file));
+        }
       }
-      this.fileMap.set(file, [...this.fileMap.get(file), processor]);
+    );
 
-      const view = this.app.workspace.getActiveViewOfType(MarkdownView);
-      if (
-        view &&
-        this.fileMap.has(file) &&
-        this.fileMap.get(file).length === 1
-      ) {
-        const self = this;
+    this.registerMarkdownPostProcessor(
+      (el: HTMLElement, ctx: MarkdownPostProcessorContext) => {
+        const file = this.app.vault.getAbstractFileByPath(ctx.sourcePath);
+        if (!file || !(file instanceof TFile)) return;
 
-        let unregisterOnUnloadFile = around(view, {
-          onUnloadFile: function (next) {
-            return async function (unloaded: TFile) {
-              if (unloaded == file) {
-                self.fileMap.delete(file);
-                unregisterOnUnloadFile();
-              }
+        const inlineRantQueryPrefix = "rant:";
+        const codeblocks = el.querySelectorAll("code");
+        for (let index = 0; index < codeblocks.length; index++) {
+          const codeblock = codeblocks.item(index);
+          const text = codeblock.innerText.trim();
+          if (text.startsWith(inlineRantQueryPrefix)) {
+            const code = text.substring(inlineRantQueryPrefix.length).trim();
 
-              return await next.call(this, unloaded);
-            };
-          },
-        });
+            const processor = new InlineRantProcessor(code, el, codeblock);
+            ctx.addChild(processor);
+            processor.rant(randomSeed());
 
-        view.register(unregisterOnUnloadFile);
-        view.register(() => this.fileMap.delete(file));
+            /* File-based tracking of registered processors inspired by javalent's excellent dice roller plugin: https://github.com/valentine195/obsidian-dice-roller */
+
+            if (!this.fileMap.has(file)) {
+              this.fileMap.set(file, []);
+            }
+            this.fileMap.set(file, [...this.fileMap.get(file), processor]);
+
+            const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+            if (
+              view &&
+              this.fileMap.has(file) &&
+              this.fileMap.get(file).length === 1
+            ) {
+              const self = this;
+
+              let unregisterOnUnloadFile = around(view, {
+                onUnloadFile: function (next) {
+                  return async function (unloaded: TFile) {
+                    if (unloaded == file) {
+                      self.fileMap.delete(file);
+                      unregisterOnUnloadFile();
+                    }
+
+                    return await next.call(this, unloaded);
+                  };
+                },
+              });
+
+              view.register(unregisterOnUnloadFile);
+              view.register(() => this.fileMap.delete(file));
+            }
+          }
+        }
       }
-    });
+    );
 
     this.addCommand({
       id: "rerant",
--- src/processor.ts
+++ src/processor.ts
@@ -1,3 +1,4 @@
+import { MarkdownRenderChild } from "obsidian";
 import { rant } from "../pkg/obsidian_rantlang_plugin.js";
 
 export default class RantProcessor {
@@ -23,6 +24,40 @@ export default class RantProcessor {
     });
 
     this.containerEl.replaceChildren(node);
+  }
+
+  rant(seed: number) {
+    this.processInput(seed);
+    this.renderResult();
+  }
+}
+
+/** Processes inline Rant blocks. */
+export class InlineRantProcessor extends MarkdownRenderChild {
+  result: string = "";
+
+  constructor(
+    public input: string,
+    public container: HTMLElement,
+    public target: HTMLElement
+  ) {
+    super(container);
+  }
+
+  processInput(seed: number) {
+    try {
+      this.result = rant(this.input, seed);
+    } catch (error) {
+      this.result = "ERROR processing Rant block (see console for details)";
+      console.error(error);
+    }
+  }
+
+  renderResult() {
+    let temp = document.createElement("span");
+    temp.appendText(this.result);
+    this.target.replaceWith(temp);
+    this.target = temp;
   }
 
   rant(seed: number) {
